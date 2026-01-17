7c342e7d20888c089f68a080f0792f92088c515f
daphnechrisitna <daphne.prem@gmail.com>
2026-01-17 22:36:46 +0530

    Make /med page dynamic

--- app/med/page.tsx
+++ app/med/page.tsx
@@ -4,6 +4,8 @@ import Header from "@/components/header"
 import HeroContent from "@/components/hero-content"
 import ShaderBackground from "@/components/shader-background"
 
+export const dynamic = 'force-dynamic'
+
 export default function MedicinePage() {
   return (
     <ShaderBackground>
